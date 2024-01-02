29377f32bcbbfad94d96be504114ab462ec45890
Sencoool <s6506021610037@email.kmutnb.ac.th>
2024-01-02 10:04:35 +0700

    Sync

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! Testing NodeMon !!!");
 });
 
 app.listen(port, () => {
